4ca19502c8af7e415361ab60ec5049990bc3d04d
KenWatanabe1520 <126440591+KenWatanabe1520@users.noreply.github.com>
2023-10-10 08:11:24 +0000

    homesコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :end_users
+
+  root to: 'homes#top'
+  module scope: :public do
+    devise_for :end_users
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
